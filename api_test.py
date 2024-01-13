412ac5b245506a5856a4305a96233d4a02a022f5
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-01-13 15:50:21 +0500

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
+client = TestClient(app, raise_server_exceptions=True)
 
 def test_root():
     response = client.get("/")
