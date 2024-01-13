23440d836db1286723a2d81ee83ec150b1dcfcf0
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-01-13 16:02:24 +0500

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,33 +1,9 @@
 from fastapi.testclient import TestClient
-from .main import app
+from main import app
 
 client = TestClient(app)
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World, input your message."}
 
-def test_generate_text():
-    item = {"text": "This is a test."}
-    response = client.post("/generate/", json=item)
-    assert response.status_code == 200
-    results = response.json()
-    assert isinstance(results, list)
-    assert len(results) > 0
-    generated_text = results[0]["generated_text"]
-    assert generated_text is not None
-
-def test_generate_text_length():
-    item = {"text": "This is a test."}
-    response = client.post("/generate/", json=item)
-    assert response.status_code == 200
-    results = response.json()
-    assert isinstance(results, list)
-    assert len(results) > 0
-    generated_texts = [result["generated_text"][:20] for result in results]
-    for generated_text in generated_texts:
-        assert len(generated_text) <= 20, f"Generated text is longer than 20 characters: {generated_text}"
 
 
 
