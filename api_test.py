486c2a4939da49ab717debd2d62e57cbffa8bbbb
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-01-13 15:25:26 +0500

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,22 +1,21 @@
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app) 
-
+client = TestClient(app)
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World, input your messege."}
+    assert response.json() == {"message": "Hello World, input your message."}
 
 def test_generate_text():
     item = {"text": "This is a test."}
     response = client.post("/generate/", json=item)
     assert response.status_code == 200
     results = response.json()
-    assert isinstance(results, list)  # Проверяем, что ответ - это список
-    assert len(results) > 0  # Проверяем, что список не пустой
-    generated_text = results[0]["generated_text"]  # Обращаемся к первому элементу списка
+    assert isinstance(results, list)
+    assert len(results) > 0
+    generated_text = results[0]["generated_text"]
     assert generated_text is not None
 
 def test_generate_text_length():
@@ -24,9 +23,9 @@ def test_generate_text_length():
     response = client.post("/generate/", json=item)
     assert response.status_code == 200
     results = response.json()
-    assert isinstance(results, list)  # Проверяем, что ответ - это список
-    assert len(results) > 0  # Проверяем, что список не пустой
-    generated_texts = [result["generated_text"][:20] for result in results]  # Получаем и обрезаем список сгенерированных текстов
+    assert isinstance(results, list)
+    assert len(results) > 0
+    generated_texts = [result["generated_text"][:20] for result in results]
     for generated_text in generated_texts:
         assert len(generated_text) <= 20, f"Generated text is longer than 20 characters: {generated_text}"
 
