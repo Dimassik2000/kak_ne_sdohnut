296787e8e4fb3c5261979161adcabeaec3e8fb22
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-01-13 16:54:14 +0500

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,24 +1,34 @@
 from fastapi.testclient import TestClient
-from fastapi import FastAPI 
-from transformers import pipeline, set_seed
-from pydantic import BaseModel
+from main import app
 
-class Item(BaseModel):
-    text: str
+client = TestClient(app) 
 
-app = FastAPI() 
-generator = pipeline('text-generation', model='openai-gpt')
-set_seed(42)
 
-@app.get("/") 
-async def root(): 
-    return {"message": "Hello World, input your messege."}
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World, input your messege."}
 
-@app.post("/generate/") 
-def generate_text(item: Item): 
-    return generator(item.text, max_length=20, num_return_sequences=5)
-  
-client = TestClient(app)
+def test_generate_text():
+    item = {"text": "This is a test."}
+    response = client.post("/generate/", json=item)
+    assert response.status_code == 200
+    results = response.json()
+    assert isinstance(results, list)  # Проверяем, что ответ - это список
+    assert len(results) > 0  # Проверяем, что список не пустой
+    generated_text = results[0]["generated_text"]  # Обращаемся к первому элементу списка
+    assert generated_text is not None
+
+def test_generate_text_length():
+    item = {"text": "This is a test."}
+    response = client.post("/generate/", json=item)
+    assert response.status_code == 200
+    results = response.json()
+    assert isinstance(results, list)  # Проверяем, что ответ - это список
+    assert len(results) > 0  # Проверяем, что список не пустой
+    generated_texts = [result["generated_text"][:20] for result in results]  # Получаем и обрезаем список сгенерированных текстов
+    for generated_text in generated_texts:
+        assert len(generated_text) <= 20, f"Generated text is longer than 20 characters: {generated_text}"
 
 
 
