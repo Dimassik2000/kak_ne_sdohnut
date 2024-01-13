9a4f8cbdbc45f8d328ec20d9b1e97fdd312ac913
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-01-13 16:04:39 +0500

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,6 +1,23 @@
 from fastapi.testclient import TestClient
-from main import app
+from fastapi import FastAPI 
+from transformers import pipeline, set_seed
+from pydantic import BaseModel
 
+class Item(BaseModel):
+    text: str
+
+app = FastAPI() 
+generator = pipeline('text-generation', model='openai-gpt')
+set_seed(42)
+
+@app.get("/") 
+async def root(): 
+    return {"message": "Hello World, input your messege."}
+
+@app.post("/generate/") 
+def generate_text(item: Item): 
+    return generator(item.text, max_length=20, num_return_sequences=5)
+  
 client = TestClient(app)
 
 
